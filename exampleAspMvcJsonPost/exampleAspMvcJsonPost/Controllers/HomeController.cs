159eea6f293a0908b187fc29c670156559852db7
Jonathan Lerín Lahuerta <j.lerin@hotmail.com>
2020-08-08 18:06:48 +0200

    First version

--- exampleAspMvcJsonPost/exampleAspMvcJsonPost/Controllers/HomeController.cs
+++ exampleAspMvcJsonPost/exampleAspMvcJsonPost/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace exampleAspMvcJsonPost.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult testJsonPost(string input)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return Json(input);
         }
     }
 }
